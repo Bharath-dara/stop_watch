1f6c1ec32bfab5db5a873df23e6ad46c94d92768
Bharath Dara <bharathdara.www@gmail.com>
2024-07-14 20:56:38 +0530

    commit

--- ministopwatch/src/App.js
+++ ministopwatch/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTime(prevTime => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+//  lskjd
+  const startStop = () => {
+    setIsRunning(prevState => !prevState);
+  };
+  const reset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  const formatTime = () => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes < 10 ? '' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="stopwatch">
+      <h1>Stopwatch</h1>
+      <h2>Time: <span>{formatTime()}</span></h2>
+      <div className="controls">
+        <button onClick={startStop}>{isRunning ? 'Stop' : 'Start'}</button>
+        <button onClick={reset}>Reset</button>
+      </div>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
